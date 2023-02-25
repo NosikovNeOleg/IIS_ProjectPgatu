e39c2eb57ea81e40aadb588977a299f57558dcc5
vanya <vanyanosov3@gmail.com>
2023-02-25 16:13:28 +0500

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import "../styles/globals.css";
 import Component from "react";
 
-function MyApp({ Component, pageProps }) {
+
+
+function MyApp({Component, pageProps } : any) {
     return (
             <>
             <Component {...pageProps} />
